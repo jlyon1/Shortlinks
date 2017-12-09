69f9d43ab9d1e785827643772b9cb8fb9668e05d
jlyon1 <joeylyon1@gmail.com>
2017-12-09 18:41:25 -0500

    Better titlebar

--- static/app.js
+++ static/app.js
@@ -136,9 +136,9 @@ Vue.component("titlebar",{
   template: `<div v-bind:style=titleStyle><p v-bind:style=paragraphStyle>{{titleText}}</p></div>`,
   data (){
     return{
-      titleStyle: {position:"absolute",backgroundColor:"#eee",height:"34px",width:"auto",top:"1",left:"0",right:"0"},
-      paragraphStyle: {float: "left",height:"34px",lineHeight:"34px",verticalAlign:"center",paddingLeft:"30px",margin:"0"},
-      titleText: "Joseph Lyon"
+      titleStyle: {position:"absolute",backgroundColor:"#eee",height:"50px",width:"auto",top:"1",left:"0",right:"0"},
+      paragraphStyle: {float: "left",height:"34px",lineHeight:"50px",verticalAlign:"center",paddingLeft:"30px",margin:"0"},
+      titleText: "Shortlinks"
     }
   }
 
