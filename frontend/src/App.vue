2c342b344e0a196e12010c81b54dffde72d725f0
jlyon1 <joeylyon1@gmail.com>
2018-02-17 14:59:06 -0500

    Begin frontend redesign

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Main></Main>
+</template>
+
+<script>
+
+import 'bulma/css/bulma.css'
+import Main from '@/components/Main'
+
+export default {
+  name: 'App',
+  components: {
+    Main
+  }
+}
+</script>
+
+<style lang="sass-loader">
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+$section-padding: 1rem;
+$family-source-sans: "Source Sans Pro", sans-serif;
+$family-primary: $family-source-sans;
+@import '~bulma/bulma';
+</style>
---
+++ frontend/src/components/Main.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+  <div class="section" style="backgroundColor:#111">
+    <div class="subtitle has-text-white">
+      Shortlinks | <a href="https://jlyon.org" style="color:white; textDecoration:none;">jlyon.org</a>
+    </div>
+  </div>
+  <div class = "section">
+    {{links}}
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      links: []
+    }
+  },
+  mounted: function(){
+    let el = this
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
